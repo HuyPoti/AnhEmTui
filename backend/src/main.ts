cdab832915cf8841f8d59a5c396d6bf8c65ded61
HuyPoti <huynguyengiapoti@gmail.com>
2026-02-07 23:21:43 +0700

    feat: initialize NestJS application with global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,14 @@ async function bootstrap() {
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
-  app.enableCors();
+  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
+  const origins = frontendUrl.split(',');
+
+  app.enableCors({
+    origin: origins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
